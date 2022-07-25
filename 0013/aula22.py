486529fd4b2d233be777b941bcd41d8b2d9a1bb9
Isabele Pinto Ferreira Silva de Oliveira <isabelepfso@gmail.com>
2022-07-25 09:38:25 -0300

    new laptop

---
+++ 0013/aula22.py
@@ -0,0 +1,9 @@
+from uteis import formulas
+
+num = int(input('Digite o número para descobrir seu fatorial: '))
+fat = formulas.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+
+x = int(input('Digite um número: '))
+dob = formulas.dobro(x)
+print(f'O fatorial de {x} é {dob}')
---
+++ 0013/uteis/formulas.py
@@ -0,0 +1,9 @@
+def fatorial(n):
+    f = 1
+    for i in range(1, n+1):
+        f *= i
+    return f
+
+
+def dobro(n):
+    return n*2
